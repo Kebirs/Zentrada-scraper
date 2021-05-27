a5d73eb8601e684cbd648f35480886b91d2b6469
Kebirs <dklecha99@gmail.com>
2021-05-27 23:53:32 +0200

    working piece of fokin code

--- zentrada_eu/products.py
+++ zentrada_eu/products.py
@@ -1,98 +1,66 @@
+import random
+import time
 from secrets import choice
-
+from functools import wraps
 import cloudscraper
 import pandas as pd
 import requests
 from lxml import html
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from requests.exceptions import RequestException
+from socket import timeout
 
 from zentrada_eu.main import Functions, DataWriter
+
+
+class Retry(object):
+    def __init__(self, times, exceptions, pause=1, retreat=1,
+                 max_pause=None, cleanup=None):
+        self.times = times
+        self.exceptions = exceptions
+        self.pause = pause
+        self.retreat = retreat
+        self.max_pause = max_pause or (pause * retreat ** times)
+        self.cleanup = cleanup
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapped_func(*args):
+            for i in range(self.times):
+                pause = min(self.pause * self.retreat ** i, self.max_pause)
+                try:
+                    return func(*args)
+                except self.exceptions:
+                    if self.pause is not None:
+                        print(f'problem nr {i} with {args}')
+                        time.sleep(pause)
+
+                    else:
+                        pass
+            if self.cleanup is not None:
+                return self.cleanup(*args)
+
+        return wrapped_func
 
 
 class Products(Functions, DataWriter):
     def __init__(self):
         super(Products, self).__init__()
         # self.scrape_all_urls()
-        # self.init_links()
-        # self.scrape_all_products()
-        self.scrape_product()
-        # self.scrape_all_prod_multithreading()
+        # self.scrape_product()
+        # self.scrape_product_1_by_1()
+        self.scrape_all_prod_multithreading()
 
-    def init_links(self):
+    def failed_call(*args, **kwargs):
+        print("Failed call: " + str(args) + str(kwargs))
 
-        all_urls = []
-        auth = {'CFID': '9e159929-0b55-4a6b-96e0-ff64e2e4623a',
-                'CFTOKEN': '0',
-                'PageNum': '1'}
-        url = 'https://www.zentrada.eu/'
-        resp = self.get_response(url, auth, '')
-
-        root_urls = "//div[@id='productgroupLeft']//a/@href"
-        root_urls = html.fromstring(resp.text).xpath(root_urls)
-        for i, url in enumerate(root_urls):
-            print(f'root {i + 1} / {len(root_urls)}')
-            static_max_page = 1000
-            for page in range(1, static_max_page):
-                auth['PageNum'] = page
-                auth['CFTOKEN'] = '0,0'
-                auth['CFID'] = ''
-                resp_3 = self.get_response(url, auth, '')
-
-                prods = "//div[@id='productlistZentrada2']//a[contains(@href, 'product.cfm')]/@href"
-                prods = html.fromstring(resp_3.text).xpath(prods)
-                if len(prods) > 0:
-                    print(f'Page {page}')
-                    for prod in prods:
-                        all_urls.append(prod)
-                        if "9e159929-0b55-4a6b-96e0-ff64e2e4623a" not in prod:
-                            print(prod)
-                else:
-                    break
-            df = pd.DataFrame(all_urls)
-            df.to_csv('zentrada_urls.csv', index=False)
-
-        df = pd.DataFrame(all_urls)
-        df.to_csv('zentrada_urls.csv', index=False)
-        return all_urls
-
-    def scrape_all_urls(self):
-        # 9e159929-0b55-4a6b-96e0-ff64e2e4623a
-        # c97e38a8-6e5d-438e-97f2-3e295ca8ea03
-
-        # 342ac053-ec02-4b8e-b2a7-8fafd9c4fa2a
-        auth = {'CFID': '342ac053-ec02-4b8e-b2a7-8fafd9c4fa2a',
-                'CFTOKEN': '0',
-                'PageNum': '1'}
-        url = 'https://www.zentrada.eu/'
-        resp = self.get_response(url, auth, self.load_proxies())
-
-        root_urls = "//div[@id='productgroupLeft']//a/@href"
-        root_urls = html.fromstring(resp.text).xpath(root_urls)
-
-        # threads, result = [], []
-        #
-        # with ThreadPoolExecutor(max_workers=30) as executor:
-        #     [threads.append(executor.submit(self.init_links, url)) for url in root_urls]
-        #
-        #     for i, task in enumerate(as_completed(threads)):
-        #         print(f"ROOT {i} / {len(threads)}")
-        #         result.extend(task.result())
-
-    def scrape_all_products(self):
-        with open(r'C:\Users\dklec\PycharmProjects\Zentrada\zentrada_eu\zentrada_urls.csv', 'r') as f:
-            urls = f.readlines()
-        urls = urls[1:]
-        urls = [x.replace('"', '').replace('\n', '') for x in urls]
-
-        for idx, url in enumerate(urls):
-            self.scrape_product(url)
-            print(f'{idx+1} / {len(urls)} |{self.status}')
-
-        self.main_output()
+    retry = Retry(times=5, pause=1, retreat=2, cleanup=failed_call,
+                  exceptions=(RequestException, timeout))
 
     @staticmethod
     def load_urls():
-        with open(r'C:\Users\dklec\PycharmProjects\Zentrada\files\urls1.txt', 'r') as f:
+        with open(r'C:\Users\dklec\PycharmProjects\Zentrada\files\urls10.txt', 'r') as f:
             urls = f.readlines()
         urls = [x.replace('\n', '') for x in urls]
         return urls
@@ -102,94 +70,106 @@ class Products(Functions, DataWriter):
 
         threads, result = [], []
 
-        with ThreadPoolExecutor(max_workers=5) as executor:
+        with ThreadPoolExecutor(max_workers=50) as executor:
             [threads.append(executor.submit(self.scrape_product, url)) for url in urls]
-
+            start = time.time()
             for i, task in enumerate(as_completed(threads)):
                 print(f"{i} / {len(threads)}")
                 task.result()
 
+            end = time.time() - start
+            print(end)
         self.main_output()
 
-    def scrape_product(self):
+    @retry
+    def scrape_product(self, url):
+        data = {}
+        s = cloudscraper.create_scraper()
+        proxy = self.load_proxies()
+
+        resp = s.get(url, proxies=proxy)
+        self.extract_product_data(data, resp, url)
+
+    @retry
+    def scrape_product_1_by_1(self):
         data = {}
         urls = self.load_urls()
         s = cloudscraper.create_scraper()
 
         for i, url in enumerate(urls):
-
             proxy = self.load_proxies()
             resp = s.get(url, proxies=proxy)
 
             print(f"{i} / {len(urls)}")
 
-            if 'wiktorowski.dev@gmail.com' in resp.text:
+            self.extract_product_data(data, resp, url)
+        self.main_output()
 
-                # print('YESSSS')
-                # continue
-                # with open('working_proxy.txt', 'a') as f:
-                #     f.write(f'{str(proxy)}\n')
+    def extract_product_data(self, data, resp, url):
 
-                category = "//div[@id='bredCrums']//text()"
+        while 'wiktorowski.dev@gmail.com' not in resp.text:
+            print('Trying different proxy')
+            s = cloudscraper.create_scraper()
+            proxy = self.load_proxies()
+            resp = s.get(url, proxies=proxy)
 
-                title = "//h1[@class='ym-mt5 ym-mb5']//text()"
-                images = "//div[@class='innerBox']//div[contains(@class, 'detailImage')]//img/@src"
+        category = "//div[@id='bredCrums']//text()"
 
-                packing_units = "//table[@class='shoppingCartTable']//td[@class='slidingB1']/text()"
-                pieces = "//table[@class='shoppingCartTable']//td[@class='slidingB2']/text()"
-                price_piece = "//table[@class='shoppingCartTable']//td[@class='slidingB3']/text()"
-                net_value = "//table[@class='shoppingCartTable']//td[@class='slidingB4']/text()"
+        title = "//h1[@class='ym-mt5 ym-mb5']//text()"
+        images = "//div[@class='innerBox']//div[contains(@class, 'detailImage')]//img/@src"
 
-                additional_details = "//table[@class='shoppingCartTable']//td[@colspan]//text()"
+        packing_units = "//table[@class='shoppingCartTable']//td[@class='slidingB1']/text()"
+        pieces = "//table[@class='shoppingCartTable']//td[@class='slidingB2']/text()"
+        price_piece = "//table[@class='shoppingCartTable']//td[@class='slidingB3']/text()"
+        net_value = "//table[@class='shoppingCartTable']//td[@class='slidingB4']/text()"
 
-                details_info_details_1 = "//div[@class='detailInfo ym-mt30']//text()"
-                details_info_details_2 = "//div[@class='detailInfo ym-mb30']//text()"
+        additional_details = "//table[@class='shoppingCartTable']//td[@colspan]//text()"
 
-                specification = "//p/../ul//li//text()"
+        details_info_details_1 = "//div[@class='detailInfo ym-mt30']//text()"
+        details_info_details_2 = "//div[@class='detailInfo ym-mb30']//text()"
 
-                details_left = "//div[@class='detailLeft']//table[@id='articleInfo']//text()"
-                details_right = "//div[@class='detailRight']//text()"
+        specification = "//p/../ul//li//text()"
 
-                category = self.extract(resp.text, category)
-                title = self.extract(resp.text, title)
-                images = html.fromstring(resp.text).xpath(images)
-                packing_units = self.extract(resp.text, packing_units)
-                pieces = self.extract(resp.text, pieces)
-                price_piece = self.extract(resp.text, price_piece)
-                net_value = self.extract(resp.text, net_value)
-                additional_details = self.extract(resp.text, additional_details)
-                details_info_details_1 = self.extract(resp.text, details_info_details_1)
-                details_info_details_2 = self.extract(resp.text, details_info_details_2)
-                specification = self.extract(resp.text, specification)
-                details_left = self.extract(resp.text, details_left)
-                details_right = self.extract(resp.text, details_right)
+        details_left = "//div[@class='detailLeft']//table[@id='articleInfo']//text()"
+        details_right = "//div[@class='detailRight']//text()"
 
-                properties = {
-                    'Link': url,
-                    'Category path': category,
-                    'Title': title,
-                    'Packing Units (PUs)': packing_units,
-                    'Pieces': pieces,
-                    'Price/Piece': price_piece,
-                    'Net Value': net_value,
-                    'Additional Details': additional_details,
-                    'Info Details 1': details_info_details_1,
-                    'Info Details 2': details_info_details_2,
-                    'Specification': specification,
-                    'Details Left': details_left,
-                    'Details Right': details_right,
-                }
+        category = self.extract(resp.text, category)
+        title = self.extract(resp.text, title)
+        images = html.fromstring(resp.text).xpath(images)
+        packing_units = self.extract(resp.text, packing_units)
+        pieces = self.extract(resp.text, pieces)
+        price_piece = self.extract(resp.text, price_piece)
+        net_value = self.extract(resp.text, net_value)
+        additional_details = self.extract(resp.text, additional_details)
+        details_info_details_1 = self.extract(resp.text, details_info_details_1)
+        details_info_details_2 = self.extract(resp.text, details_info_details_2)
+        specification = self.extract(resp.text, specification)
+        details_left = self.extract(resp.text, details_left)
+        details_right = self.extract(resp.text, details_right)
 
-                for k, v in properties.items():
-                    data[k] = v
+        properties = {
+            'Link': url,
+            'Category path': category,
+            'Title': title,
+            'Packing Units (PUs)': packing_units,
+            'Pieces': pieces,
+            'Price/Piece': price_piece,
+            'Net Value': net_value,
+            'Additional Details': additional_details,
+            'Info Details 1': details_info_details_1,
+            'Info Details 2': details_info_details_2,
+            'Specification': specification,
+            'Details Left': details_left,
+            'Details Right': details_right,
+        }
 
-                for idx, x in enumerate(images):
-                    data[f'Image {idx+1}'] = x
+        for k, v in properties.items():
+            data[k] = v
 
-                self.products_output(data)
+        for idx, x in enumerate(images):
+            data[f'Image {idx + 1}'] = x
 
-            else:
-                print('NO')
+        self.products_output(data)
 
     @staticmethod
     def load_proxies():
